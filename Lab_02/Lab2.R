965e15a28734474d1cc1e5ba402c645c73223003
Conner Engle <17256046+cengle12@users.noreply.github.com>
2023-03-07 13:52:31 -0800

    Update Lab2.R

--- Lab_02/Lab2.R
+++ Lab_02/Lab2.R
@@ -1,1 +1,15 @@
+# Author: Conner Engle
+# Johns Hopkins: AS.410.671.82.SU22
+
+# Data visualization using Spellman data
+
+# Spellman, P. T., Sherlock, G., Zhang, M. Q., Iyer, V. R., Anders, K., Eisen, M. B., Brown, P. O., Botstein, D., & Futcher, B. (1998). 
+# Comprehensive identification of cell cycle-regulated genes of the yeast Saccharomyces cerevisiae by microarray hybridization. 
+# Molecular biology of the cell, 9(12), 3273–3297. 
+# https://doi.org/10.1091/mbc.9.12.3273
+
+
+#######################
+#          1          #
+####################### 
 
